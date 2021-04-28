2cb81c79b55e92c6189b6c91c9b9537d6a8d17b5
trgith <desarrollo@trnetwork.com.mx>
2021-04-28 11:50:08 -0500

    cambios

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
 const express = require('express');
-const app = express();
-app.use(requireHTTPS);
-
-app.use(express.static('./dist/<name-on-package.json>'));
-
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
-  );
+const path = require('path');
+const ngApp = express();
+ngApp.use(express.static('./dist/angular-forms-validation'));
+ngApp.get('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, '/dist/angular-forms-validation/index.html'));
 });
-
-app.listen(process.env.PORT || 8080);
+ngApp.listen(process.env.PORT || 8080);
