9a9448cf27bfe9a73d7219064103087adf5a1e84
adano1997 <endlessknow1997@outlook.com>
2021-05-11 15:41:34 -0500

    Mas cambios jsjss

--- src/app/dashboard/dashboard2/dashboard2.component.ts
+++ src/app/dashboard/dashboard2/dashboard2.component.ts
@@ -3,6 +3,7 @@ import swal from 'sweetalert2';
 import { Observable, timer, interval } from 'rxjs';
 import { take, map } from 'rxjs/operators';
 import { NgbModal, ModalDismissReasons, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { transpileModule } from 'typescript';
 
 declare var require: any;
 @Component({
@@ -21,6 +22,10 @@ export class Dashboard2Component{
   contadorActivo: boolean;
   instrucciones: boolean;
   solicitarChat: boolean;
+  pantallaAgradecimiento: boolean;
+  botonEmergencia: boolean;
+  porcentajeActual: boolean;
+  titulo: boolean;
 
 
   constructor(private modalService: NgbModal) { }
@@ -32,7 +37,10 @@ export class Dashboard2Component{
     this.primeraPregunta = false;
     this.segundaPregunta = false;
     this.contadorActivo = false;
-
+    this.pantallaAgradecimiento = false;
+    this.botonEmergencia = false;
+    this.porcentajeActual = false;
+    this.titulo = false;
   }
 
   DobleValidacion(){
@@ -40,10 +48,11 @@ export class Dashboard2Component{
   }
 
   LimiteTiempo(){
-    interval(1000).subscribe((val) => { console.log(this.restante$); });
+    interval(1000).subscribe((val) => {  });
   }
 
   ComenzarCuestionario() {
+    this.botonEmergencia = true;
     this.primeraPregunta = true;
     this.contadorActivo = true;
     this.instrucciones = false;
@@ -55,8 +64,11 @@ export class Dashboard2Component{
   }
 
   ComenzarCuestionarioIntegral() {
+    this.porcentajeActual = true;
+    this.botonEmergencia = true;
     this.primeraPregunta = true;
     this.instrucciones = false;
+    this.titulo = true;
   }
 
   //Esto es del Modal
@@ -68,5 +80,25 @@ export class Dashboard2Component{
   //Modal de Ajustes como tamaño de letra
   Ajustes(ajustes){
     this.modalService.open(ajustes, { windowClass: 'dark-modal' });
+  }
+
+  //Modal de Solicitud de Ayuda
+  SolicitarAyuda(ayuda){
+    this.modalService.open(ayuda, { windowClass: 'dark-modal' });
+  }
+
+  //Funcion Cambiar Tamaño de Fuente
+  CambiarTamanoLetra(){
+    var e = document.getElementById("exampleFormControlSelect1");
+    document.getElementById('PrimeraPregunta').style.fontSize = e.value;
+  }
+
+  ExpulsionDeEmergencia(){
+    this.pantallaAgradecimiento = true;
+    this.instrucciones = false;
+    this.primeraPregunta = false;
+    this.segundaPregunta = false;
+    this.contadorActivo = false;
+    this.botonEmergencia = false;
   }
 }
